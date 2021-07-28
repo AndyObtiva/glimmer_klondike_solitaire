32d286b72b21baecabee507f5c817afa5f45f564
Andy Maleh <andy.am@gmail.com>
2021-07-28 10:56:08 -0400

    Refactoring/simplification of card shapes and fixing issue with disposing shapes in dealt pile

--- app/glimmer_klondike_solitaire/view/empty_playing_card.rb
+++ app/glimmer_klondike_solitaire/view/empty_playing_card.rb
@@ -4,12 +4,8 @@ class GlimmerKlondikeSolitaire
       include Glimmer::UI::CustomShape
   
       body {
-        rectangle(0, 0, 50, 80) {
-          background :dark_green
-          
-          rectangle(0, 0, 49, 79, 15, 15) {
-            foreground :gray
-          }
+        rectangle(0, 0, 49, 79, 15, 15) {
+          foreground :gray
         }
       }
   
--- app/glimmer_klondike_solitaire/view/hidden_playing_card.rb
+++ app/glimmer_klondike_solitaire/view/hidden_playing_card.rb
@@ -4,12 +4,10 @@ class GlimmerKlondikeSolitaire
       include Glimmer::UI::CustomShape
   
       body {
-        rectangle(0, 0, 50, 80) {
-          background :dark_green
+        rectangle(0, 0, 49, 79, 15, 15) {
+          background :red
           
-          rectangle(0, 0, 49, 79, 15, 15) {
-            background :red
-          }
+          # border
           rectangle(0, 0, 49, 79, 15, 15) {
             foreground :black
           }
